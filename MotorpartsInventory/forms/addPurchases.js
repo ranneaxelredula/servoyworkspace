31d0d8eefea2ca6490d88d69b4275a6cda70b60f
ranneaxelredula <79342212+ranneaxelredula@users.noreply.github.com>
2021-02-26 12:30:15 +0800

    Add files via upload

---
+++ MotorpartsInventory/forms/addPurchases.js
@@ -0,0 +1,10 @@
+
+/**
+ * @properties={typeid:24,uuid:"4C0958E2-C463-47BF-885E-060CC472B221"}
+ */
+function addPurchaseSupplier() {
+	// TODO Auto-generated method stub
+	databaseManager.startTransaction();
+	foundset.newRecord();
+	databaseManager.saveData();
+}
